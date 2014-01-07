5b13374b4591a0311a8e4f78578b44264d6f9a88
Jaime Hablutzel <hablutzel1@gmail.com>
2014-01-07 15:35:42 -0500

    Reapplying patch from a7ce1097f40e5d173deb53cd98dbeceb14ce5778

--- framework/src/play/templates/FastTags.java
+++ framework/src/play/templates/FastTags.java
@@ -137,7 +137,7 @@ public class FastTags {
 	    if (args.containsKey("name")) {
             name = args.get("name").toString();
         }
-        if (!("GET".equals(actionDef.method) || "POST".equals(actionDef.method))) {
+        if (!("GET".equalsIgnoreCase(actionDef.method) || "POST".equalsIgnoreCase(actionDef.method))) {
             String separator = actionDef.url.indexOf('?') != -1 ? "&" : "?";
             actionDef.url += separator + "x-http-method-override=" + actionDef.method.toUpperCase();
             actionDef.method = "POST";
